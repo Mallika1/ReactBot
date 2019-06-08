36da110313ad9aa10db87acfb85c2c7f958a9b7b
Mallika <mailtomallika2009@gmail.com>
2019-06-07 17:38:19 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
-const express = require('express');
+const express = require("express");
 
-// Tells node that we are creating an "express" server
-var app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.get("/", function(req, res) {
-    res.send({"hello" : "there"});
-  });
-
-  app.post("/api/textquery", function(req, res) {
-    res.send({"hello" : "there1"});
-  });
-
-  app.post("/api/eventquery", function(req, res) {
-    res.send({"hello" : "there2"});
-  });
-
-
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
